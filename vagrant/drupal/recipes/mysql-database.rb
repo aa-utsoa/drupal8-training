8fe481abb098047dc6bd67a805c742b7daa3d59f
Bastian Widmer <b.widmer@dasrecht.net>
2014-04-16 21:01:46 +0200

    TRAINING-27 fixing version 1.0.0

--- vagrant/drupal/recipes/mysql-database.rb
+++ vagrant/drupal/recipes/mysql-database.rb
@@ -4,3 +4,10 @@ bash "Create Database" do
     echo "create database IF NOT EXISTS drupal" | mysql
     EOH
 end
+
+bash "Import Database" do
+  user "root" # see attributes section below
+  code <<-EOH
+    echo "source /home/vagrant/public_html/assets/vanilla-drupal8/drupal_20140415_113317.sql" | mysql --database=drupal
+    EOH
+end
