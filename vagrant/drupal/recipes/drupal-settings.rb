9b51557a653e7337ec603277dfee230af5915d92
Bastian Widmer <b.widmer@dasrecht.net>
2014-04-17 13:35:53 +0200

    TRAINING-27 - files directory chmod fixed

--- vagrant/drupal/recipes/drupal-settings.rb
+++ vagrant/drupal/recipes/drupal-settings.rb
@@ -15,8 +15,12 @@ directory "/home/vagrant/public_html/drupal8-training/sites/default/" do
   mode "0755"
 end
 
+directory "/home/vagrant/public_html/drupal8-training/sites/default/files" do
+  mode "0777"
+end
 
-%w[ files files/config_vagrant files/config_vagrant/staging].each do |dir|
+
+%w[ files/config_vagrant files/config_vagrant/staging].each do |dir|
   directory "/home/vagrant/public_html/drupal8-training/sites/default/#{dir}" do
     mode 00755
   end
